1ab6e909fcd7f75e7bb25b0e2889814ff64ea6cf
cyw3 <2927096163@qq.com>
2016-04-20 14:50:17 +0800

    swordOffer

--- LeetCode/src/com/cyw/swordoffer/MinNumberInRotateArray.java
+++ LeetCode/src/com/cyw/swordoffer/MinNumberInRotateArray.java
@@ -6,10 +6,19 @@ public class MinNumberInRotateArray {
 	 * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
 	 * 输入一个非递减序列的一个旋转，输出旋转数组的最小元素。
 	 * 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
-	 * @param array
+	 * @param array 旋转数组
 	 * @return
 	 */
     public int minNumberInRotateArray(int [] array) {
-		return 0;
+    	int len = array.length;
+    	if(array==null||len==0){  
+    		return 0;
+    	}
+    	int i=len-1;
+    	for(;i>0;i--){
+    		if(array[i-1]>array[i])
+    			break;
+    	}
+		return array[i];
     }
 }
