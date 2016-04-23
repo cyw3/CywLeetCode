f36beabd7a550f320f86cdea6077d80422f042bf
cyw3 <2927096163@qq.com>
2016-04-24 00:02:58 +0800

    How to BST

---
+++ LeetCode/src/com/cyw/swordoffer/PermutationOfString.java
@@ -0,0 +1,24 @@
+package com.cyw.swordoffer;
+
+import java.util.ArrayList;
+
+/**
+ * 字符串的排列
+ * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。
+ * 例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串
+ * abc,acb,bac,bca,cab和cba。 结果请按字母顺序输出。 
+输入描述:
+输入一个字符串,长度不超过9(可能有字符重复),字符只包括大小写字母。
+ * @author cyw
+ *
+ */
+public class PermutationOfString {
+	/**
+	 * 递归。子问题
+	 * @param str
+	 * @return
+	 */
+    public ArrayList<String> Permutation(String str) {
+        
+    }
+}
